cf755cacad9bf341baa1bfe2c5ba968ffbf2bc5b
Bibo <zyprex0711@gmail.com>
2025-08-03 08:00:11 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist'
-  }
-});
+  // On ajoute l'option base pour le déploiement sur GitHub Pages.
+  // Cela garantit que les chemins d'accès sont correctement gérés en production.
+  base: '/marcher-ensemble/'
+})
