4df76d3a4b3d2fb5aaa49a5a83cd56c32568edca
Bibo <zyprex0711@gmail.com>
2025-08-03 08:02:49 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // On ajoute l'option base pour le déploiement sur GitHub Pages.
-  // Cela garantit que les chemins d'accès sont correctement gérés en production.
-  base: '/marcher-ensemble/'
+  // L'option 'base' a été supprimée pour éviter les conflits avec le déploiement sur Vercel.
 })
