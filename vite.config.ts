77c29c2ce0402491e9e19ee5a842223fbe3195a9
Bibo <zyprex0711@gmail.com>
2025-08-03 08:10:09 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // L'option 'base' a été supprimée pour éviter les conflits avec le déploiement sur Vercel.
 })
